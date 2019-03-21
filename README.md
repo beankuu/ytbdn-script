7807e37095154bef6ce42ac1518b7237d0190a04
beankuu <beankuu@gmail.com>
2019-03-20 22:56:13 -0600

    Initial push

---
+++ README.md
@@ -0,0 +1,5 @@
+# ytbdn-script
+
+Simple bat script for downloading <b>youtube mp3</b> in <b>Windows</b>
+
+requirements: youtube-dl
---
+++ ytbdn.bat
@@ -0,0 +1,17 @@
+@Echo off
+echo ============================
+echo      Youtube Downloader
+echo ============================
+echo.
+echo.
+
+:runName
+set /p link="link: "
+if /I "%link%" Equ "q" goto :end
+  start youtube-dl --quiet --extract-audio --audio-format mp3 %link%
+echo.
+goto :runName
+
+:end
+echo byebye
+exit
